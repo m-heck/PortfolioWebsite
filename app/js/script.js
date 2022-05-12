256fd8b41ee5d3ca66e374946e38d7d337d25758
Maren Heck <marenjheck@gmail.com>
2022-05-12 18:44:55 -0400

    adding timeline

--- app/js/script.js
+++ app/js/script.js
@@ -23,3 +23,17 @@ links.forEach((link) => {
 /*var x = e.clientX;
   var y = e.clientY;
   cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px), 0)`*/
+
+/* ================ CURSOR ================ */
+const parentContainer = document.getElementsByClassName('read-more-container');
+
+for (let i = 0; i < parentContainer.length; i++) {
+  parentContainer[i].addEventListener('click', event=>{
+    const current = event.target;
+    const isReadMoreBtn = current.className.includes('read-more-button');
+    if(!isReadMoreBtn) return;
+    const currentText = event.target.parentNode.querySelector('.read-more-text');
+    currentText.classList.toggle('read-more-text--show');
+    current.textContent = current.textContent.includes('Read More') ? "Read Less..." : "Read More...";
+  })
+}
