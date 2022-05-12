5ca6989bcc269cd6b0a292930191145815eacbbf
Maren Heck <marenjheck@gmail.com>
2022-05-12 19:20:57 -0400

    transitions

--- app/js/script.js
+++ app/js/script.js
@@ -37,3 +37,33 @@ for (let i = 0; i < parentContainer.length; i++) {
     current.textContent = current.textContent.includes('Read More') ? "Read Less..." : "Read More...";
   })
 }
+
+/* ============= TRANSITIONS ============= */
+/* Fade in */
+
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+  //threshold: 1, // waits for the entire thing to be in the screen
+  //rootMargin: "0px 0px -100px 0px"
+};
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add('appear');
+      appearOnScroll.unobserve(entry.target);
+    }
+  })
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+})
+
+/* slide in */
+const sliders = document.querySelectorAll('.slide-in');
+sliders.forEach(slider => {
+  appearOnScroll.observe(slider);
+})
