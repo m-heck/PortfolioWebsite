be974587529f3def2380fecc5f601c197c7a6fc9
Maren Heck <marenjheck@gmail.com>
2022-05-12 15:53:37 -0400

    cursor style

--- app/js/script.js
+++ app/js/script.js
@@ -7,39 +7,13 @@ document.addEventListener('mousemove', (e) => {
   cursor.style.top = e.pageY + 'px';
 })
 
-function invisible(div){
-  div.style.fontSize = "0";
-}
 
-/*
-
-function addEvent(obj, evt, fn) {
-  if (obj.addEventListener) {
-      obj.addEventListener(evt, fn, false);
-  }
-  else if (obj.attachEvent) {
-      obj.attachEvent("on" + evt, fn);
-  }
-}
-
-addEvent(document, "mouseout", function(e) {
-  e = e ? e : window.event;
-  var from = e.relatedTarget || e.toElement;
-  if (!from || from.nodeName == "HTML") {
-      cursor.style.width = '0';
-  }
+let links = Array.from(document.querySelectorAll("a"));
+links.forEach((link) => {
+  link.addEventListener("mouseover", () => {
+    cursor.classList.add("grow");
+  });
+  link.addEventListener("mouseleave", () => {
+    cursor.classList.remove("grow");
+  });
 });
-*/
-
-// ================ JQUERY ================
-
-var script = document.createElement('script');
-script.src = 'https://code.jquery.com/jquery-3.6.0.min.js';
-document.getElementsByTagName('head')[0].appendChild(script);
-
-/*
-$(".down").click(function() {
-  $([document.documentElement, document.body]).animate({
-      scrollTop: $("#project2").offset().top
-  }, 2000);
-});*/
